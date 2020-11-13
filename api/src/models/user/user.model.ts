e24ded0be14cfb4f3f9b0f3963782b9a730d5b72
davideastmond <davebuck1981@gmail.com>
2020-11-12 22:47:00 -0500

    Create User schema

---
+++ api/src/models/user/user.model.ts
@@ -0,0 +1,8 @@
+/**
+ * Brings the interface and schema together for a type-checked mongoose model
+ */
+import { model } from "mongoose";
+import { IUserDocument, IUserModel } from "./user.types";
+import UserSchema from "./user.schema";
+
+export const UserModel = model<IUserDocument, IUserModel>("user", UserSchema);
