fb89397ea35921b568943f81e129e6035deee641
davideastmond <davebuck1981@gmail.com>
2020-12-03 11:24:39 -0500

    removed .only from test

--- api/src/db/utils/chunk-list.test.ts
+++ api/src/db/utils/chunk-list.test.ts
@@ -1,6 +1,6 @@
 import { chunkList } from "./chunk-list";
 
-describe.only("chunk tests", () => {
+describe("chunk tests", () => {
   test("function chunks arrays properly as per limit and offset", () => {
     const testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 ];
 
