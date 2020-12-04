131045febcf1dba09128b4b047424690f34beef1
lily-law <lilylaw.dev@gmail.com>
2020-12-04 15:55:47 +0000

    Change SERVER_PORT to PORT for Heroku

--- src/server.ts
+++ src/server.ts
@@ -21,7 +21,7 @@ const app = express();
 connectDB();
 
 // Express configuration
-app.set("port", process.env.SERVER_PORT || 7000);
+app.set("port", process.env.PORT || 7000);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
