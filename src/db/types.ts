c435e529d02fc4db2420cfc98cb1edeb7611b526
lily-law <lilylaw.dev@gmail.com>
2021-01-17 15:30:31 +0000

    Convert profile data ids and dates to strings

--- src/db/types.ts
+++ src/db/types.ts
@@ -1,7 +1,43 @@
-import { IThreadCommentReference } from "../models/thread-comment/thread-comment.types";
-import { IThreadReactionReference } from "../models/thread-reaction/thread-reaction.types";
-import { IThreadReference } from "../models/thread/thread.types";
-import { IUserConnection } from "../models/user-connection/user-connection.types";
+import { IThreadFork } from "../models/thread-fork/thread-fork.types";
+import { ThreadReactionTypeTitle } from "../models/thread-reaction/thread-reaction.types";
+import { ThreadVisibility } from "../models/thread/thread.types";
+import { Types } from "mongoose";
+
+export interface IProfileConnection {
+  userId: string | Types.ObjectId;
+  dateTimeConnected: string | Date;
+  isTeamMate: boolean;
+}
+
+export interface IProfileThreadsReference {
+  threadId: string | Types.ObjectId;
+  createdAt: string | Date;
+  updatedAt: string | Date;
+  contentSnippet: string;
+  postedByUserId: string | Types.ObjectId;
+}
+
+export interface IProfileThreadsCommentReference {
+  threadData: IProfileThreadsReference;
+  commentData: {
+    commentId: string | Types.ObjectId;
+    createdAt: string | Date;
+    updatedAt: string | Date;
+    contentSnippet: string;
+    postedByUserId: string | Types.ObjectId;
+  };
+}
+
+export interface IProfileThreadsReactionReference {
+  threadData: IProfileThreadsReference;
+  reactionData: {
+    reactionId: string | Types.ObjectId;
+    postedByUserId: string | Types.ObjectId;
+    title: ThreadReactionTypeTitle;
+    createdAt: string | Date;
+    updatedAt: string | Date;
+  };
+}
 
 export interface IProfile {
   id: string;
@@ -11,12 +47,14 @@ export interface IProfile {
   avatarUrls: Array<{url: string}>;
   nOfConnections: number;
   isAConnection: boolean;
-  connections: { [userId: string]: IUserConnection };
-  connectionOf: { [userId: string]: IUserConnection };
+  connections: { [userId: string]: IProfileConnection };
+  connectionOf: { [userId: string]: IProfileConnection };
   threads: {
-    started: { [threadId: string]: IThreadReference };
-    commented: { [threadId: string]: { [commentId: string]: IThreadCommentReference }};
-    reacted: { [threadId: string]: { [reactionId: string]: IThreadReactionReference } };
+    started: { [threadId: string]: IProfileThreadsReference };
+    commented: { [threadId: string]: { [commentId: string]: IProfileThreadsCommentReference }};
+    reacted: { [threadId: string]: { [reactionId: string]: IProfileThreadsReactionReference } };
   };
   isCurrentUser: boolean;
+  updatedAt: string;
+  createdAt: string;
 }
--- src/db/utils/get-profile-by-id/get-profile-by-id.ts
+++ src/db/utils/get-profile-by-id/get-profile-by-id.ts
@@ -1,5 +1,6 @@
 import { UserModel } from "../../../models/user/user.model";
 import { IProfile } from "../../../db/types";
+
 /**
  * Finds user by ID and only returns relevant properties
  * @param userId ObjectId
@@ -16,9 +17,77 @@ export async function getProfileById({userId, reqUserId}: {userId: string, reqUs
     avatarUrls: result.avatarUrls,
     nOfConnections: Object.keys(result.connections).length, 
     isAConnection: Object.keys(result.connectionOf).includes(reqUserId),
-    connections: result.connections,
-    connectionOf: result.connectionOf,
-    threads: result.threads,
+    connections: parseConnectionsData(result.connections),
+    connectionOf: parseConnectionsData(result.connectionOf),
+    threads: parseThreadsData(result.threads),
     isCurrentUser: userId === reqUserId,
+    updatedAt: result.updatedAt.valueOf().toString(),
+    createdAt: result.createdAt.valueOf().toString(),
   };
+}
+
+function parseConnectionsData(connections: IProfile["connections"]) {
+  const newConnections = {...connections};
+  Object.entries(connections).forEach(([key, value]) => {
+    newConnections[key] = {
+      ...value, 
+      userId: value.userId.toString(),
+      dateTimeConnected: value.dateTimeConnected.valueOf().toString()
+    };
+  });
+  return newConnections
+}
+
+function parseThreadsData(threads: IProfile["threads"]) {
+  const newThreads = {...threads};
+  Object.entries(threads.started).forEach(([key, value]) => {
+    newThreads.started[key] = {
+      ...value, 
+      threadId: value.threadId.toString(),
+      createdAt: value.createdAt.valueOf().toString(),
+      updatedAt: value.updatedAt.valueOf().toString(),
+      postedByUserId: value.postedByUserId.toString()
+    };
+  });
+  Object.entries(threads.commented).forEach(([key, value]) => {
+    Object.entries(value).forEach(([commentKey, commentValue]) => {
+      newThreads.commented[key][commentKey] = {
+        threadData: {
+          ...commentValue.threadData, 
+          threadId: commentValue.threadData.threadId.toString(),
+          createdAt: commentValue.threadData.createdAt.valueOf().toString(),
+          updatedAt: commentValue.threadData.updatedAt.valueOf().toString(),
+          postedByUserId: commentValue.threadData.postedByUserId.toString()
+        },
+        commentData: {
+          ...commentValue.commentData,
+          commentId: commentValue.commentData.commentId.toString(),
+          createdAt: commentValue.commentData.createdAt.valueOf().toString(),
+          updatedAt: commentValue.commentData.updatedAt.valueOf().toString(),
+          postedByUserId: commentValue.commentData.postedByUserId.toString()
+        }
+      };
+    });
+  });
+  Object.entries(threads.reacted).forEach(([key, value]) => {
+    Object.entries(value).forEach(([reactionKey, reactionValue]) => {
+      newThreads.reacted[key][reactionKey] = {
+        threadData: {
+          ...reactionValue.threadData, 
+          threadId: reactionValue.threadData.threadId.toString(),
+          createdAt: reactionValue.threadData.createdAt.valueOf().toString(),
+          updatedAt: reactionValue.threadData.updatedAt.valueOf().toString(),
+          postedByUserId: reactionValue.threadData.postedByUserId.toString()
+        },
+        reactionData: {
+          ...reactionValue.reactionData,
+          reactionId: reactionValue.reactionData.reactionId.toString(),
+          createdAt: reactionValue.reactionData.createdAt.valueOf().toString(),
+          updatedAt: reactionValue.reactionData.updatedAt.valueOf().toString(),
+          postedByUserId: reactionValue.reactionData.postedByUserId.toString()
+        }
+      };
+    });
+  });
+  return newThreads
 }
