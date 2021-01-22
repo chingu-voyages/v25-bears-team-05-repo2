ec60317aeec31ec8caa4fb2e6d03ca7713eefc00
davideastmond <davebuck1981@gmail.com>
2021-01-22 09:32:33 -0500

    fixed import issues

--- src/models/user/google-user-helper/google-user-helper.ts
+++ src/models/user/google-user-helper/google-user-helper.ts
@@ -1,28 +1,28 @@
-import { IGoogleOauthProfile } from "google-oath-profile";
 import { IUser } from "../user.types";
 import { encrypt } from "../../../utils/crypto";
+import { IGoogleOauthProfile } from "../../../types/google-oath-profile";
 
-export function createUserFromGoogleData (profile: IGoogleOauthProfile): IUser {
+export function createUserFromGoogleData(profile: IGoogleOauthProfile): IUser {
   return {
     firstName: profile.name.givenName,
-      lastName: profile.name.familyName,
-      auth: {
-        googleId: profile.id,
-        email: encrypt(profile.emails[0].value.toLowerCase()),
-        oauth: profile.accessToken
-      },
-      avatar: profile.photos.map((photo) => {
-        return { url: photo.value } ;
-      }),
-      connections: {},
-      connectionOf: {},
-      threads: {
-        started: {},
-        commented: {},
-        liked: {},
-        shared: {},
-      },
-      createdAt: new Date(),
-      updatedAt: new Date()
+    lastName: profile.name.familyName,
+    auth: {
+      googleId: profile.id,
+      email: encrypt(profile.emails[0].value.toLowerCase()),
+      oauth: profile.accessToken,
+    },
+    avatar: profile.photos.map((photo) => {
+      return { url: photo.value };
+    }),
+    connections: {},
+    connectionOf: {},
+    threads: {
+      started: {},
+      commented: {},
+      liked: {},
+      shared: {},
+    },
+    createdAt: new Date(),
+    updatedAt: new Date(),
   };
 }
