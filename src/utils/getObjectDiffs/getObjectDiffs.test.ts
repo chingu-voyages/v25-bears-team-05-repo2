c000900656f886d1006a925b0bb60060405ccb82
lily-law <lilylaw.dev@gmail.com>
2021-02-11 16:36:38 +0000

    Update getObjectDIffs test

--- src/utils/getObjectDiffs/getObjectDiffs.test.ts
+++ src/utils/getObjectDiffs/getObjectDiffs.test.ts
@@ -1,33 +1,39 @@
 import { Types } from "mongoose";
 import getObjectDiffs from "./getObjectDiffs";
 
-const base = {
-    id: Types.ObjectId(),
-    unnestedString: "one",
-    nested: {
-        string: "Hello world"
-    },
-    updatedAt: Date.now()
-};
 const object = {
-    id: Types.ObjectId(),
-    unnestedString: "two",
-    nested: {
-        string: "Hi world"
-    },
-    updatedAt: Date.now()
+    content: { html: 'some kind of new content here' },
+    threadType: 'photo',
+    visibility: 1,
+    _id: Types.ObjectId("60254ba04d60ff708b01f664"),
+    postedByUserId: Types.ObjectId("60254ba04d60ff708b01f661"),
+    createdAt: new Date("2021-02-11T15:22:08.071Z"),
+    updatedAt: new Date("2021-02-11T15:22:08.223Z"),
+    __v: 0
 };
+const base = {
+    _id: Types.ObjectId("60254ba04d60ff708b01f664"),
+    threadType: 'article',
+    visibility: 0,
+    postedByUserId: Types.ObjectId("60254ba04d60ff708b01f661"),
+    content: { html: 'someSampleHTML' },
+    createdAt: new Date("2021-02-11T15:22:08.071Z"),
+    updatedAt: new Date("2021-02-11T15:22:08.071Z"),
+    __v: 0
+}
 
 describe("Compares only one unnested property", () => {
-    const propertiesWhiteList = ["unnestedString"];
+    const propertiesWhiteList = ["threadType"];
     const diffs = getObjectDiffs(object, base, propertiesWhiteList);
     it("Has one property", () => expect(Object.keys(diffs)).toHaveLength(1));
-    it("Has the whitelisted field name", () => expect(diffs).toHaveProperty("unnestedString"));
-    it("As a string", () => expect(diffs["unnestedString"]).toEqual(expect.any(String)));
+    it("Has the whitelisted field name", () => expect(diffs).toHaveProperty("threadType"));
+    it("As a string", () => expect(diffs["threadType"]).toEqual(expect.any(String)));
+    it("Output looks correct", () => expect(diffs["threadType"]).toBe('<span class="removed">ar</span><span class="added">pho</span>t<span class="removed">icle</span><span class="added">o</span>'))
 });
 
 describe("Compares nested property", () => {
-    const propertiesWhiteList = ["nested", "string"];
+    const propertiesWhiteList = ["content", "html"];
     const diffs = getObjectDiffs(object, base, propertiesWhiteList);
-    it("As a nested string", () => expect(diffs["nested"]["string"]).toEqual(expect.any(String)));
+    it("As a nested string", () => expect(diffs["content"]["html"]).toEqual(expect.any(String)));
+    it("Output looks correct", () => expect(diffs["content"]["html"]).toBe('some<span class="removed">Sampl</span><span class="added"> kind of n</span>e<span class="removed">HTML</span><span class="added">w content here</span>'))
 });
