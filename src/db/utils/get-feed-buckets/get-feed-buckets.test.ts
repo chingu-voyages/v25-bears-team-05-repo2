b61b963b3533c849cb8e637e4ac0b5d56df1175c
lily-law <lilylaw.dev@gmail.com>
2021-02-12 10:31:48 +0000

    Outline buckets tests [WIP]

---
+++ src/db/utils/get-feed-buckets/get-feed-buckets.test.ts
@@ -0,0 +1,32 @@
+
+describe("Buckets", async () => {
+    it("Returns with correct structure", async () => {
+
+    });
+    it("Has bucket items with correct structure", async () => {
+
+    });
+    it("Gives bucket items a priority relative to current user", async () => {
+    
+    })
+    describe("getFeedBucket with prop {latestBucketRecieved: 0}", () => {
+        it("Returns the lastest set of buckets", async () => {
+    
+        });
+    });
+    describe("getFeedBucket with prop {latestBucketRecieved: <date from a few buckets in>}", () => {
+        it("Returns the lastest set of buckets after latestBucketRecieved date", async () => {
+    
+        });
+    });
+    describe("getFeedBucket with prop {oldestBucketRecieved: <date from a few buckets in>}", () => {
+        it("Returns the lastest set of buckets", async () => {
+    
+        });
+    });
+    describe("getFeedBucket with prop {oldestBucketRecieved: <date of oldest bucket>}", () => {
+        it("Returns bucket with empty collection object as there are no more buckets", async () => {
+    
+        });
+    });
+});
