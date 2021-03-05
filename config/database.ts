1a0c942b655fa3ff485e88b205531ef08255a720
David Eastmond <davebuck1981@gmail.com>
2021-03-05 09:10:06 -0500

    database URI changes depending on dev or production mode

--- config/database.ts
+++ config/database.ts
@@ -3,7 +3,13 @@ import { ConnectionOptions, connect } from "mongoose";
 
 const connectDB = async () => {
   try {
-    const mongoURI: string = process.env.DEV_MONGO_DB_URI;
+    const isProduction =
+      process.env.NODE_ENV && process.env.NODE_ENV.match("production");
+
+    let mongoURI = process.env.DEV_MONGO_DB_URI;
+    if (isProduction) {
+      mongoURI = process.env.PRODUCTION_MONGO_DB_URI;
+    }
     const options: ConnectionOptions = {
       useNewUrlParser: true,
       useCreateIndex: true,
@@ -11,7 +17,9 @@ const connectDB = async () => {
       useUnifiedTopology: true,
     };
     await connect(mongoURI, options);
-    console.log("MongoDB Connected...");
+    console.log(
+      `MongoDB Connected... ${isProduction ? "production" : "development"}`
+    );
   } catch (err) {
     console.error(err.message);
     // Exit process with failure
