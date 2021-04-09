ee00db586842548f04917dfcb66c07913babd0d5
David Eastmond <davebuck1981@gmail.com>
2021-04-09 06:36:08 -0400

    moved captcha validation to middleware

--- src/routes/recover.ts
+++ src/routes/recover.ts
@@ -1,53 +1,26 @@
-// import { createError } from "../utils/errors";
 import express from "express";
-const router = express.Router();
-import assert from "assert";
 import { body, validationResult } from "express-validator/check";
-import axios from "axios";
-import { createError } from "../utils/errors";
-const isProduction =
-  process.env.NODE_ENV && process.env.NODE_ENV.match("production");
+const router = express.Router();
 
-const captchaSecretKey = isProduction
-  ? process.env.PRODUCTION_CAPTCHA_SECRET_KEY
-  : process.env.DEV_CAPTCHA_SECRET_KEY;
-assert(captchaSecretKey, "captcha secret key is not defined.");
-
+import { validateCaptcha } from "../middleware/password-recovery/validate-captcha";
 const sanitizationObject = [
   body("email").isEmail().normalizeEmail({ all_lowercase: true }),
   body("captcha").exists().not().isEmpty(),
 ];
 
-router.post("/", sanitizationObject, async (req: any, res: any) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    res.statusMessage = JSON.stringify(errors.array());
-    return res.status(400).end();
-  }
-  const verifyUrl = `https://google.com/recaptcha/api/siteverify?secret=${captchaSecretKey}&response=${req.body.captcha}&remoteip=${req.connection.remoteAddress}`;
-  try {
-    const response = await axios.post(verifyUrl);
-    if (response.data.success === true) {
-      res.status(200).json({ response: "ok" });
-      // Do some logic
-    } else {
-      res.statusMessage =
-        "Captcha challenge was not successful. Please try again.";
+router.post(
+  "/",
+  sanitizationObject,
+  validateCaptcha,
+  async (req: any, res: any) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.statusMessage = JSON.stringify(errors.array());
       return res.status(400).end();
     }
-  } catch (err) {
-    res.status(400).send({
-      errors: [
-        {
-          ...createError(
-            "password recovery",
-            `captcha verification error. ${err}`,
-            "captcha"
-          ),
-        },
-      ],
-    });
+    console.log("Captcha is validated");
+    res.status(200).send({ response: "ok " }); // More processing to be done
   }
-});
+);
 
 export default router;
