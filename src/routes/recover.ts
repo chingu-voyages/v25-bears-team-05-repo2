1fdb15f95f15ddb6246707e5f91a3317a34c97c2
David Eastmond <davebuck1981@gmail.com>
2021-04-12 19:59:31 -0400

    implement route and basic response

--- src/routes/recover.ts
+++ src/routes/recover.ts
@@ -3,6 +3,8 @@ import { body, validationResult } from "express-validator/check";
 const router = express.Router();
 
 import { validateCaptcha } from "../middleware/password-recovery/validate-captcha";
+import { createRequest } from "../models/password-recovery/password-recovery.methods";
+import { createError } from "../utils/errors";
 const sanitizationObject = [
   body("email").isEmail().normalizeEmail({ all_lowercase: true }),
   body("captcha").exists().not().isEmpty(),
@@ -18,8 +20,22 @@ router.post(
       res.statusMessage = JSON.stringify(errors.array());
       return res.status(400).end();
     }
-    console.log("Captcha is validated");
-    res.status(200).send({ response: "ok " }); // More processing to be done
+    const requestData = {
+      emailId: req.body.email,
+      requestorIpAddress: req.connection.remoteAddress
+    }
+    try {
+      const response = await createRequest(requestData);
+      res.status(200).send({ response: "ok ", requestData: response }); // More processing to be done
+    } catch (error) {
+      res
+      .status(400)
+      .send({
+        errors: [
+          { ...createError("password recovery request error", `database error. ${error}`, "n/a") },
+        ],
+      });
+    }
   }
 );
 
