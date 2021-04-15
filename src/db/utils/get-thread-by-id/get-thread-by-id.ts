116b301f0a12f31fb8d6e39955bbf89510b4ae44
Lily <lilylaw.dev@gmail.com>
2021-04-15 15:25:20 +0100

    Update src/db/utils/get-thread-by-id/get-thread-by-id.ts
    Co-authored-by: David Eastmond <davebuck1981@gmail.com>

--- src/db/utils/get-thread-by-id/get-thread-by-id.ts
+++ src/db/utils/get-thread-by-id/get-thread-by-id.ts
@@ -1,7 +1,7 @@
 import { ThreadModel } from "../../../models/thread/thread.model";
 /**
  * Finds user by ID and only returns relevant properties
- * @param userId ObjectId
+ * @param threadId ObjectId
  */
 export async function getThreadById({threadId}: {threadId: string}) {
   return await ThreadModel.findById(threadId);
