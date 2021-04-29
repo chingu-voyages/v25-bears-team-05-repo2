55f73741bea1899b050a3b528a88d7fb737b105d
David Eastmond <davebuck1981@gmail.com>
2021-04-29 09:55:19 -0400

    added gmail flag to validation

--- src/routes/register-local.ts
+++ src/routes/register-local.ts
@@ -10,15 +10,22 @@ import { encrypt } from "../utils/crypto";
 
 passport.use("local", LocalPassportStrategy);
 
-const sanitizationObject = [ body("email").isEmail().normalizeEmail({ all_lowercase: true }),
-  body("password").isLength({ min: 8}).trim().escape(),
+const sanitizationObject = [
+  body("email")
+    .isEmail()
+    .normalizeEmail({ all_lowercase: true, gmail_remove_dots: false }),
+  body("password").isLength({ min: 8 }).trim().escape(),
   body("firstName").trim().escape(),
-  body("lastName").trim().escape() ];
+  body("lastName").trim().escape(),
+];
 
 const router = express.Router();
 
-router.post("/", sanitizationObject, validatePassword,
-  async(req: Request, res: Response) => {
+router.post(
+  "/",
+  sanitizationObject,
+  validatePassword,
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).send({ errors: errors.array() });
@@ -28,25 +35,27 @@ router.post("/", sanitizationObject, validatePassword,
       firstName: req.body.firstName,
       lastName: req.body.lastName,
       encryptedEmail: encrypt(req.body.email),
-      plainTextPassword: req.body.password
+      plainTextPassword: req.body.password,
     };
 
     try {
       const result = await UserModel.registerUser(userDetails);
       if (result) {
-       req.logIn(result, (err) => {
-         if (err) { console.log(err); }
-         return res.redirect("/register/local/success");
-       });
+        req.logIn(result, (err) => {
+          if (err) {
+            console.log(err);
+          }
+          return res.redirect("/register/local/success");
+        });
       }
     } catch (err) {
       if (err.toString().match(/Error: User with email \w*/)) {
         res.status(409).send(err);
-      }
-      else {
+      } else {
         res.status(400).send(err);
       }
     }
-});
+  }
+);
 
 export default router;
