fb45582709775cb7e5deb340e48a10c2774c3f1c
lily-law <lilylaw.dev@gmail.com>
2021-05-17 16:48:50 +0100

    Fix small bugs in comment route methods - Export comment route and add to server file

--- src/routes/comments.ts
+++ src/routes/comments.ts
@@ -3,16 +3,15 @@ import { Response } from "express";
 import { body, param } from "express-validator/check";
 import { routeProtector } from "../middleware/route-protector";
 import { createError } from "../utils/errors";
-import { ThreadModel } from "../models/thread/thread.model";
 import { handleChainValidationResult } from "../middleware/handle-chain-validation-result";
 import { ThreadCommentModel } from "../models/thread-comment/thread-comment.model";
 const router = express.Router();
 
 const handleGetComments = async (req: any, res: Response) => {
     try {
-        const commentIds = req.params.id; // TODO: Check this works! I think this should return an array but need to try, url query should be: ?id=<commentId>&id=<commentId>&...
-        const result = await ThreadModel.find({ _id: { $in: commentIds } });
-        return res.status(200).send({ threadComments: result});
+        const commentIds = req.query.id; // TODO: Check this works! I think this should return an array but need to try, url query should be: ?id=<commentId>&id=<commentId>&...
+        const result = await ThreadCommentModel.find({ _id: { $in: commentIds } });
+        return res.status(200).send({ threadComments: result });
     } catch (err) {
         res.status(400).send({ errors: [{ 
             ...createError("Unable to get comments",
@@ -24,17 +23,18 @@ const handleGetComments = async (req: any, res: Response) => {
 router.get("/", routeProtector, handleGetComments);
 
 const postCommentValidationChain = [
-    param("thread_id").exists().trim().escape(),
+    body("threadId").exists().trim().escape(),
     body("content").exists().trim().escape(),
     body("attachments").custom((value) => !value || Array.isArray(value))
 ];
 const handlePostComment = async(req: any, res: Response) => {
     try {
         const result = await req.user.addThreadComment({
-        targetThreadId: req.params.thread_id,
-        threadCommentData: {
-            content: req.body.content, attachments: req.body.attachments
-        }
+            targetThreadId: req.body.threadId,
+            threadCommentData: {
+                content: req.body.content, 
+                attachments: req.body.attachments,
+            }
         });
         return res.status(200).send(result);
     } catch (err) {
@@ -63,9 +63,9 @@ const handleDeleteComment = async(req: any, res: Response) => {
         }]});
     }
 };
-router.delete("/:commentId", deleteCommentValidationChain, handleChainValidationResult, handleDeleteComment);
+router.delete("/:comment_id", deleteCommentValidationChain, handleChainValidationResult, handleDeleteComment);
 
-const getCommentValidationChain = [param("commentId").exists().trim().escape()];
+const getCommentValidationChain = [param("comment_id").exists().trim().escape()];
 const handleGetComment = async(req: any, res: Response) => {
     try {
         const result = await ThreadCommentModel.findById(req.params.commentId);
@@ -78,4 +78,6 @@ const handleGetComment = async(req: any, res: Response) => {
         }]});
     }
 };
-router.get("/:commentId", getCommentValidationChain, handleChainValidationResult, handleGetComment);
+router.get("/:comment_id", getCommentValidationChain, handleChainValidationResult, handleGetComment);
+
+export default router;
