18d2c3d7b36d537dd83d587c4f544ef091bc1966
David Eastmond <davebuck1981@gmail.com>
2021-06-25 21:05:37 -0400

    removed unneeded function

--- src/models/user/user-test-helper/user-test-helper.ts
+++ src/models/user/user-test-helper/user-test-helper.ts
@@ -40,38 +40,3 @@ export function createTestUsers(input: {
   }
   return fakeUsers;
 }
-
-export function createTestUsersWithSequentialID(input: {
-  numberOfUsers: number;
-  googleIds?: Array<string>;
-  plainTextPasswords?: Array<string>;
-}): Array<IUser> {
-  assert(input.numberOfUsers > 0, "number of users must be greater than 0");
-  const fakeUsers: Array<IUser> = [];
-  for (let i = 0; i < input.numberOfUsers; i++) {
-    fakeUsers.push({
-      _id: mongoose.Types.ObjectId((100000000000 + i + 1).toString()),
-      firstName: `testUser${i.toString()}FirstName`,
-      lastName: `testUser${i.toString()}LastName`,
-      jobTitle: `testUser${i.toString()}JobTitle`,
-      auth: {
-        password: input.plainTextPasswords && input.plainTextPasswords[i],
-        googleId: input.googleIds && input.googleIds[i],
-        email: encrypt(`testUser${i.toString()}@test.com`),
-      },
-      avatar: [{ url: `testUser${i.toString()}AvatarUrl` }],
-      connections: {},
-      connectionRequests: {},
-      notifications: [],
-      threads: {
-        started: {},
-        commented: {},
-        liked: {},
-        shared: {},
-      },
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    });
-  }
-  return fakeUsers;
-}
