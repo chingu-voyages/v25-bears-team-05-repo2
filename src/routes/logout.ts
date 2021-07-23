ffb9fa67610b08d4746147f92deccc48faa3c9ec
Lily Law <lilylaw.dev@gmail.com>
2021-07-23 16:14:50 +0100

    Hotfix remove routeProtector from logout route

--- src/routes/logout.ts
+++ src/routes/logout.ts
@@ -1,9 +1,8 @@
 import * as express from "express";
 import { Request, Response } from "express";
-import { routeProtector } from "../middleware/route-protector";
 const router = express.Router();
 
-router.post("/", routeProtector, (req: Request, res: Response) => {
+router.post("/", (req: Request, res: Response) => {
   req.logOut();
   res.status(200).send({ message: "Logged out successfully"}); // Placeholder response
 });
